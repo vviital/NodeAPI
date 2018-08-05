7b2592f97948b564be0b864c9ea093525b2749f5
vviital <vviital@tut.by>
2018-08-05 11:35:47 +0300

    Change way of working with env

--- libs/config.js
+++ libs/config.js
@@ -1,15 +1,14 @@
 var nconf = require('nconf');
 
-nconf.argv().env();
+nconf.use('memory')
 
-if (process.env.ENV_IN === 'docker') {
-    nconf.file('docker', {
-        file: process.cwd() + '/docker.config.json'
-    });
-}
-
-nconf.file('defaults', {
-    file: process.cwd() + '/config.json'
-});
+nconf.set('port', +process.env.PORT || 1337);
+nconf.set('security:tokenLife', +process.env.TOKEN_LIFE || 3600);
+nconf.set('mongoose:uri', process.env.MONGOOSE_URI || 'mongodb://localhost/apiDB');
+nconf.set('default:user:username', process.env.DEFAULT_USER_NAME || 'myapi');
+nconf.set('default:user:password', process.env.DEFAULT_USER_PASSWORD || 'abc1234');
+nconf.set('default:client:name', process.env.DEFAULT_CLIENT_NAME || 'Android API v1');
+nconf.set('default:client:clientId', process.env.DEFAULT_CLIENT_ID || 'android');
+nconf.set('default:client:clientSecret', process.env.DEFAULT_CLIENT_SECRET || 'SomeRandomCharsAndNumbers');
 
 module.exports = nconf;
