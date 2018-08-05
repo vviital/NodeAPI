bd5e11de6c25d3c19d90fedde3a5afb25b037739
vviital <vviital@tut.by>
2018-08-05 11:47:01 +0300

    Add env for travis

--- libs/config.js
+++ libs/config.js
@@ -2,13 +2,24 @@ var nconf = require('nconf');
 
 nconf.use('memory')
 
-nconf.set('port', +process.env.PORT || 1337);
-nconf.set('security:tokenLife', +process.env.TOKEN_LIFE || 3600);
-nconf.set('mongoose:uri', process.env.MONGOOSE_URI || 'mongodb://localhost/apiDB');
-nconf.set('default:user:username', process.env.DEFAULT_USER_NAME || 'myapi');
-nconf.set('default:user:password', process.env.DEFAULT_USER_PASSWORD || 'abc1234');
-nconf.set('default:client:name', process.env.DEFAULT_CLIENT_NAME || 'Android API v1');
-nconf.set('default:client:clientId', process.env.DEFAULT_CLIENT_ID || 'android');
-nconf.set('default:client:clientSecret', process.env.DEFAULT_CLIENT_SECRET || 'SomeRandomCharsAndNumbers');
+if (process.env.NODE_ENV === 'travis-test') {
+    nconf.set('port', 1337);
+    nconf.set('security:tokenLife', 3600);
+    nconf.set('mongoose:uri', 'mongodb://localhost/apiDB');
+    nconf.set('default:user:username', 'myapi');
+    nconf.set('default:user:password', 'abc1234');
+    nconf.set('default:client:name', 'Android API v1');
+    nconf.set('default:client:clientId', 'android');
+    nconf.set('default:client:clientSecret', 'SomeRandomCharsAndNumbers');
+} else {
+    nconf.set('port', +process.env.PORT);
+    nconf.set('security:tokenLife', +process.env.TOKEN_LIFE);
+    nconf.set('mongoose:uri', process.env.MONGOOSE_URI);
+    nconf.set('default:user:username', process.env.DEFAULT_USER_NAME);
+    nconf.set('default:user:password', process.env.DEFAULT_USER_PASSWORD);
+    nconf.set('default:client:name', process.env.DEFAULT_CLIENT_NAME);
+    nconf.set('default:client:clientId', process.env.DEFAULT_CLIENT_ID);
+    nconf.set('default:client:clientSecret', process.env.DEFAULT_CLIENT_SECRET);
+}
 
 module.exports = nconf;
